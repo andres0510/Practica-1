a77b7f07a095442b639cc86a67cad5f9deef7bbc
User <j.andres0510@hotmail.com>
2017-10-04 22:36:46 -0500

    Validación de gramática actualizado

--- src/controlador/LeerGuardar.java
+++ src/controlador/LeerGuardar.java
@@ -19,82 +19,77 @@ public class LeerGuardar {
     
     public String leerTxt() throws IOException{                             
         fc = new JFileChooser();     
-        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);                                           //Buscar solo archivos de texto
+        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);                                                   //Buscar solo archivos de texto
         fc.addChoosableFileFilter(new FileNameExtensionFilter("Archivos Txt","txt"));
         fc.setAcceptAllFileFilterUsed(false);                                                       
-        fc.showOpenDialog(fc);                                                                      //Mostrar selector de archivos
-        try{                                                                                        //Leer el archivo de texto
+        fc.showOpenDialog(fc);                                                                              //Mostrar selector de archivos
+        try{                                                                                                //Leer el archivo de texto
                 String nombreArchivo = fc.getSelectedFile().getAbsolutePath();
                 archivoTxt = new FileInputStream(nombreArchivo);
                 entrada = new DataInputStream(archivoTxt);
-                br = new BufferedReader(new InputStreamReader(entrada,"iso-8859-1"));               //Admitir caracteres especiales
+                br = new BufferedReader(new InputStreamReader(entrada,"iso-8859-1"));                       //Admitir caracteres especiales
                 String aux;
                 while((aux=br.readLine())!= null){
                     texto = texto + aux + "\n";
                 }
                 entrada.close();
-                texto = texto.replaceAll(" ", "");                                                  //Eliminar espacios en la gramatica
+                texto = texto.replaceAll(" ", "");                                                          //Eliminar espacios en la gramatica
                 return texto;
-        } catch(FileNotFoundException e){                                                           //Error al cargar el archivo
+        } catch(FileNotFoundException e){                                                                   //Error al cargar el archivo
             JOptionPane.showMessageDialog(null, "Ha ocurrido un error al leer el archivo");
             return null;
         }
     }
     
-    public boolean esLetra(int ascii){                                                              //Identificar si el caracter es una letra
+    public boolean esLetra(int ascii){                                                                      //Identificar si el caracter es una letra
         if(ascii<65 || (ascii>90 && ascii < 97) || ascii > 122){
             return false;                        
         }
         return true;
     }
     
-    public boolean esNumero(int ascii){                                                             //Identificar si el caracter es numero
+    public boolean esNumero(int ascii){                                                                     //Identificar si el caracter es numero
         if(ascii<48 && ascii>57){
             return false;
         }
         return true;
     }
     
-    public boolean validarGram(String gram){                                                        //Verificar la validez la de gramatica                                                      
-        char token='\n';                                                                            //Token analizado anteriormente
-        char tokenAux;
+    public boolean validarGram(String gram){                                                                //Verificar la validez la de gramatica                                                      
+        char token='\n';                                                                                
+        char tokenAux;                                                                                      //Token analizado anteriormente
         int largo = gram.length();
-        boolean llaveAbierta=false;                                                                 //Identificar si hay un "<" sin cerrar
-        boolean derecha=false;                                                                      //Identificar si está en la parte derecha de la gramatica
-        boolean hayNoTerm=false;
+        boolean llaveAbierta=false;                                                                         //Identificar si hay un "<" sin cerrar
+        boolean derecha=false;                                                                              //Identificar si está en la parte derecha de la gramatica
         int i=0;
         int ascii;
-        int asciiTokenAux;                                                                          //Para verificar el último carater en el case default
+        int asciiTokenAux;                                                                                  //Para verificar el último carater en el case default
         while(i<largo){
             tokenAux=token;
             token = gram.charAt(i);
             ascii = gram.codePointAt(i);
             switch(tokenAux){
-                case '\n':  if(token != '<' && token != '\n' && !esLetra(ascii) && !esNumero(ascii)){
+                case '\n':  if(token != '<' && token != '\n'){
                                 return false;
                             }                              
                             if(token == '<'){
                                 llaveAbierta = true;
                             }
                             derecha=false;
-                            hayNoTerm=false;
                             break;
-                case '<' :  if(!derecha){
-                                hayNoTerm=true;
-                            }
-                            if(!esLetra(ascii)){                                                            //En un no terminal solo se admiten letras
+                case '<' :  if(!esLetra(ascii)){                                                            //En un no terminal solo se admiten letras
                                 return false;                        
                             }
                             break;
-                case '>' :  if (!llaveAbierta){
+                case '>' :  if(!llaveAbierta){
                                 return false;
                             }
-                            if (!derecha){                                                                  //Si está a la izquierda solo puede pasar a la derecha o abrir otro no terminal
-                                if(token != '=' && token != '<' && !esLetra(ascii) && !esNumero(ascii)){
+                            if(!derecha){                                                                   //Si está a la izquierda, solo puede pasar a la derecha al cerrar el NT
+                                if(token != '='){
                                     return false;     
                                 }
-                            } else{                                                                         //Si está a la derecha
-                                if(!esLetra(ascii) && !esNumero(ascii) && token != '\n' && token != '<'){   //Solo se permiten letras, números, abrir un no terminal o salto de línea
+                            } else{                                                                         
+                                if(!esLetra(ascii) && !esNumero(ascii) && token != '\n' && token != '<'){   //Solo se permiten letras, números, abrir un NT o salto de línea
                                     return false;                        
                                 }
                             }
@@ -103,13 +98,10 @@ public class LeerGuardar {
                                 llaveAbierta = true;
                             }
                             break;
-                case '=' :  if (derecha){                                                                   //Si ya se encuentra a la derecha
+                case '=' :  if(derecha){                                                                    //Si ya se encuentra a la derecha
                                 return false;
                             }
-                            if(!hayNoTerm){
-                                return false;
-                            }
-                            if (token != '<' && !esLetra(ascii) && !esNumero(ascii) && token != '/'){
+                            if(token != '<' && !esLetra(ascii) && !esNumero(ascii) && token != '/'){
                                 return false;
                             }
                             if(token == '<'){
@@ -117,40 +109,31 @@ public class LeerGuardar {
                             }
                             derecha=true;
                             break;
-                case '/' :  if (token != '\n'){                                                             //Si es la secuencia nula solo se permite pasar a otra línea
+                case '/' :  if(token != '\n'){                                                              //Si es la secuencia nula solo se permite pasar a otra línea
                                 return false;
                             }
                             break;
                 default :   asciiTokenAux = (int) tokenAux;
-                            if (token == '<' && llaveAbierta){
+                            if(token == '<' && llaveAbierta){
                                     return false;
                                 }
                             if(esNumero(asciiTokenAux)){
-                                if(!derecha){
-                                    if(!esNumero(ascii) && token != '=' && token != '<'){
-                                        return false;
-                                    }
-                                } else{
-                                    if(!esNumero(ascii) && token != '\n' && token != '<'){
-                                        return false;
-                                    }
+                                if(!esNumero(ascii) && !esLetra(ascii) && token != '\n' && token != '<'){
+                                    return false;
                                 }
                             } else{
                                 if(esLetra(asciiTokenAux)){
-                                    if (token == '>' && !llaveAbierta){
+                                    if(token == '>' && !llaveAbierta){
                                         return false;
                                     }
-                                    if (token == '>'){
+                                    if(token == '>'){
                                         llaveAbierta=false;
                                     }
-                                    if (esNumero(ascii) && llaveAbierta){
+                                    if(esNumero(ascii) && llaveAbierta){
                                         return false;
                                     }
                                     if(!derecha){
-                                        if(!esNumero(ascii) && !esLetra(ascii) && token != '=' && token != '<' && token != '>'){
-                                            return false;
-                                        }
-                                        if(token == '=' && llaveAbierta){
+                                        if(!esLetra(ascii) && token != '>'){
                                             return false;
                                         }
                                     } else{
@@ -172,7 +155,7 @@ public class LeerGuardar {
             }
             i++;
         }
-        if(llaveAbierta || (!derecha && token != '\n')){
+        if(llaveAbierta || (!derecha && token != '\n')){                                                    //Si hay una llave abierta o si la izquierda está incompleta
             return false;
         }
         return true;
